54c0e3f35e607ead22cc83398424500827d8f99f
Yonkov,Atanas A <a.yonkov@student.fontys.nl>
2020-05-21 16:24:45 +0200

    Update js/custom-scripts/login-check-redirect.js, register.html, style.css files

--- js/custom-scripts/login-check-redirect.js
+++ js/custom-scripts/login-check-redirect.js
@@ -1,31 +1,25 @@
 // script used for login.html validation
 function check() {
 
-   var email = document.getElementById("email").value;
-   var username = document.getElementById("usrname").value;
-   var password = document.getElementById("psw").value;
-   var loggedIn = false;
+    //    var email = document.getElementById("email").value;
+    var username = document.getElementById("usrname").value;
+    var password = document.getElementById("psw").value;
 
     // stored data from the register-form
-    var storedEmail = localStorage.getItem("email");
+    // var storedEmail = localStorage.getItem("email");
     var storedName = localStorage.getItem("username");
     var storedPw = localStorage.getItem("password");
 
-
-
     // check if stored data from register-form is equal to data from login form
-    if (email == storedEmail && password == storedPw && username == storedName) 
-    {
-
-
-        alert('You are logged in.');
+    if (password == storedPw && username == storedName) {
+        alert('Login succesful!');
         window.location.replace("http://i426654.hera.fhict.nl/student%20html/MyProfile.html");
     }
-       
-     else {
-        alert('ERROR.');
+
+    else {
+        alert('Username or password invalid');
     }
-    
+
 }
- 
+
 
